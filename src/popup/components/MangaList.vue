a494a443744cbe77ed55e012963701a0967ec7d3
Pierre-Louis Duhoux <pl.duhoux@gmail.com>
2018-03-29 22:27:38 -0300

    Sorting manga list

--- src/popup/components/MangaList.vue
+++ src/popup/components/MangaList.vue
@@ -7,12 +7,26 @@
         <!-- Once loaded -->
         <div v-if="loaded">
             <div v-if="allMangas.length" class="amr-mangas">
+                <div class="amr-filters-container">
+                    <v-tooltip top content-class="icon-ttip">
+                        <v-icon slot="activator" @click="sort = 'az'" :class="'amr-filter ' + (sort === 'az' ? 'activated' : '')">mdi-sort-alphabetical</v-icon>
+                        <span>Sort mangas in alphabetical order</span>
+                    </v-tooltip>
+                    <v-tooltip top content-class="icon-ttip">
+                        <v-icon slot="activator" @click="sort = 'updates'" :class="'amr-filter ' + (sort === 'updates' ? 'activated' : '')">mdi-flash</v-icon>
+                        <span>New chapters to read first !</span>
+                    </v-tooltip>
+                </div>
                 <!-- Categories -->
-                <Categories :categories="categories" :static-cats="false" :delegate-delete="false" />
+                <div class="amr-categories-container">
+                    <Categories :categories="categories" :static-cats="false" :delegate-delete="false" />
+                </div>
                 <!-- Load manga list -->
                 <div class="amr-manga-list-container">
-                    <MangaGroup v-if="options.groupmgs === 0"  v-for="mg in allMangas" v-bind:key="mg.key" :mangas="[mg]" />
-                    <MangaGroup v-if="options.groupmgs !== 0"  v-for="(grp, key) in groupedMangas" v-bind:key="key" :mangas="grp" />
+                    <transition-group name="flip-list" tag="div">
+                        <MangaGroup v-if="options.groupmgs === 0"  v-for="mg in allMangas" v-bind:key="mg.key" :mangas="[mg]" />
+                        <MangaGroup v-if="options.groupmgs !== 0"  v-for="(grp, key) in groupedMangas" v-bind:key="key" :mangas="grp" />
+                    </transition-group>
                 </div>
             </div>
             <!-- No mangas yet -->
@@ -33,12 +47,14 @@ import { mapGetters } from "vuex";
 import MangaGroup from "./MangaGroup";
 import Categories from "./Categories";
 import browser from "webextension-polyfill";
-import * as utils from '../../amr/utils';
+import * as utilsamr from '../../amr/utils';
+import * as utils from '../utils';
 
 export default {
   data() {
     return {
-      loaded: false
+      loaded: false,
+      sort: "updates" // sort mode for list (az : alphabetical, updates : mangas with updates first)
     };
   },
   computed: {
@@ -50,13 +66,24 @@ export default {
         return this.options.categoriesStates;
     },
     groupedMangas: function() {
+        // sort mangas
+        let sort = this.sort;
+        let sorted = this.allMangas.sort(function(a, b) {
+            if (sort === "az") {
+                return a.name < b.name ? -1 : (a.name === b.name ? 0 : 1);
+            } else if (sort === "updates") {
+                let ha = utils.hasNew(a),
+                    hb = utils.hasNew(b);
+                // primary sort on manga has new chapter, secondary on alphabetical
+                return (ha === hb ? (a.name < b.name ? -1 : (a.name === b.name ? 0 : 1)) : (ha && !hb ? -1 : 1));
+            }
+        });
         // create groups
-        var groups = this.allMangas.reduce((grps, manga) => {
-            let key = utils.formatMgName(manga.name);
+        return sorted.reduce((grps, manga) => {
+            let key = utilsamr.formatMgName(manga.name);
             (grps[key] = grps[key] || []).push(manga);
             return grps;
         }, {});
-        return groups; // no more string index
     },
     ...mapGetters(["countMangas", "allMangas"])
   },
@@ -93,5 +120,25 @@ export default {
   margin: 20px;
   text-align: center;
   width: 100%;
+}
+.amr-filters-container {
+    float: right;
+    margin-top: 10px;
+    margin-right: 10px;
+}
+.amr-categories-container {
+    margin-right: 44px;
+}
+.amr-filter {
+    font-size: 20px;
+    color:grey;
+    margin-left: 4px;
+    cursor: pointer;
+}
+.amr-filter.activated {
+    color: black;
+}
+.flip-list-move {
+  transition: transform 1s;
 }
 </style>
