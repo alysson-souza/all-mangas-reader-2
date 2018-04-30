9eb9d5075f66ff6cc5650b7f849bcf930dec1e34
Pierre-Louis Duhoux <pl.duhoux@gmail.com>
2018-04-30 19:02:34 +0200

    [FIX] Option page did not load mirrors implementations from extension page

--- src/pages/options/App.vue
+++ src/pages/options/App.vue
@@ -1,7 +1,12 @@
 <template>
 	<v-app :dark="$store.state.options.dark === 1">
         <v-content>
-            <Options />
+          <!-- Before mirrors and mangas are loaded into options -->
+          <div v-if="!loaded" class="amr-loader">
+              <v-progress-circular indeterminate :width="4" :size="50" color="red darken-2"></v-progress-circular>
+          </div>
+          <!-- Once loaded -->
+          <Options v-if="loaded" />
         </v-content>
 	</v-app>
 </template>
@@ -12,8 +17,27 @@ import Options from "../components/Options";
 
 export default {
   data() {
-    return {};
+    return {
+      loaded: false
+    };
   },
+  async created() {
+    let init = [];
+    // initialize mirrors state for store in options from background
+    init.push(this.$store.dispatch("getStateFromReference", {
+      module: "mirrors",
+      key: "all",
+      mutation: "setMirrors"
+    }));
+    // initialize mangas state for store in options from background
+    init.push(this.$store.dispatch("getStateFromReference", {
+      module: "mangas",
+      key: "all",
+      mutation: "setMangas"
+    }));
+    await Promise.all(init);
+    this.loaded = true
+	}, 
   methods: {
     i18n: (message, ...args) => i18n(message, ...args),
   },
@@ -22,5 +46,9 @@ export default {
 };
 </script>
 <style>
-
+.amr-loader {
+  margin: 20px;
+  text-align: center;
+  width: 100%;
+}
 </style>
