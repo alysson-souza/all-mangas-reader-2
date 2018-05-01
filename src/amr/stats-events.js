7aa0331b254254b0a620571b2b6dbde7d9138f96
Pierre-Louis Duhoux <pl.duhoux@gmail.com>
2018-05-01 11:11:34 +0200

    [FIX] Recording statistics issue

--- src/amr/stats-events.js
+++ src/amr/stats-events.js
@@ -2,19 +2,23 @@ import store from '../store';
 
 class StatsEvents {
     constructor() {
-        window.dataLayer = window.dataLayer || [];
-        this.gtag('js', new Date());
-        this.gtag('config', 'UA-118453067-1');
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+        
+        ga('create', 'UA-118453067-1', 'auto');
+        ga('set', 'checkProtocolTask', function(){}); // Removes failing protocol check. @see: http://stackoverflow.com/a/22152353/1958200
+        ga('require', 'displayfeatures');
+        ga('send', 'pageview', this.getCurPage());
     }
-    gtag() {
-        dataLayer.push(arguments);
+    getCurPage() {
+        let lp = window.location.href.lastIndexOf("/");
+        return window.location.href.substring(lp);
     }
     trackEvent(category, action, label) {
         try {
-            this.gtag('event', action, {
-                'event_category': category,
-                'event_label': label
-            });
+            ga('send', 'event', category, action, label);
         } catch (e) {
             if (store.state.options.debug === 1) {
                 console.error("Error while recording statistics : ")
