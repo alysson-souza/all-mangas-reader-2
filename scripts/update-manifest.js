639a2b79dd856e21907613694985487c354c49a2
Julien "_FrnchFrgg_" RIVAUD <frnchfrgg@free.fr>
2018-08-16 13:06:06 +0200

    Add version sanity checking capabilities to update-manifest

--- scripts/update-manifest.js
+++ scripts/update-manifest.js
@@ -1,6 +1,7 @@
 /**
  * Update the manifest.json file from dist folder to create a releasable extension : 
  * Options are : 
+ *  -checkver x: prior to anything else, check if the version in the manifest is equal to the given argument, and fail with exit code 255 if not
  *  -patch x : number that will be added to the current version number (if current version is 2.0.0 and x is 67, generated version will be 2.0.0.67). If is specified, the generated extension will be supposed to update on the beta channel and "Beta" will be added to the extension name
  *  -chrome : if option is present, manifest will be adapted for chrome needs
  *  -firefox : if option is present, manifest will be adapted for firefox needs
@@ -9,6 +10,14 @@ const fs = require('fs');
 const path = require('path');
 let fileName = path.join(__dirname, '../dist/manifest.json');
 let ext = require(fileName);
+
+if (process.argv.includes("-checkver")) {
+    let ver = process.argv[process.argv.findIndex(el => el === "-checkver") + 1];
+    if (ver != ext.version) {
+        console.log("Checking version failed: expected '" + ver + "', got '" + ext.version + "'.")
+        process.exit(255);
+    }
+}
 
 // update file entries
 let isBeta = false;
