6362e6dc1187704774ca0943cd71db28b4a596b9
Isotab <batosi05@gmail.com>
2019-11-13 16:08:16 -0500

    A little cleanup and fixing comicake

--- src/mirrors/mirrors/ComiCake.js
+++ src/mirrors/mirrors/ComiCake.js
@@ -3,20 +3,24 @@
  */
 window["ComiCake"] = function(options) {
     this.default_options = {
-        series_list_url: "/directory/",
-        listchaps_reverse: true 
+        series_list_url: '/directory/',
+        series_list_selector: '.mdc-card__media-title a',
+        chapter_list_selector: 'a[href*="/read/"]',
+        chapter_information_selector: '.mdc-toolbar__section .etext a',
+        images_selector: 'main img',
+        chapter_determine_selector: 'div#br-slider img',
+        chapter_determine_strip_selector: 'main#cakeClassic'
     }
     this.options = Object.assign(this.default_options, options)
     this.mirrorName = "ComiCake"
     this.canListFullMangas = true
-    this.chapter_url = /^\/read\/.*\/.*$/g
 
     this.getMangaList = async function (search) {
         let self = this
         let res = []
         let doc = await amr.loadPage(this.options.reader_url + this.options.series_list_url)
 
-        $('.mdc-card__media-title a', doc).each(function() {
+        $(this.options.series_list_selector, doc).each(function() {
             res.push([
                 $(this).text().trim(),
                 self.options.reader_url + $(this).attr('href')
@@ -30,44 +34,66 @@ window["ComiCake"] = function(options) {
         let doc = await amr.loadPage(urlManga, { nocache: true, preventimages: true })
         let res = []
         let self = this
-        $("a[href*='/read/']", doc).each(function (index) {
+        $(this.options.chapter_list_selector, doc).each(function (index) {
             res.push([
                 $(this).text(), 
                 self.options.reader_url + $(this).attr("href")
             ])
         })
-        if (this.options.listchaps_reverse) {
-            res = res.reverse()
-        }
+        
         return res
     }
 
     this.getInformationsFromCurrentPage = async function (doc, curUrl) {
-        let link = $('div#br-topbar > .br-toolbar__tsection a', doc)
+        doc = await this.getStripMode(doc, curUrl)
+        let link = $(this.options.chapter_information_selector, doc)
         return {
             "name" : link.text(),
-            "currentMangaURL" : link.attr('href'),
+            "currentMangaURL" : this.options.reader_url + link.attr('href'),
             "currentChapterURL" : curUrl
         }
     }
 
     this.getListImages = async function (doc, curUrl) {
         let res = []
-        $('div#br-slider img', doc).each(function() {
+        doc = await this.getStripMode(doc, curUrl)
+        $(this.options.images_selector, doc).each(function() {
             res.push($(this).attr('src'))
         })
         return res
-    },
+    }
 
     this.getImageFromPageAndWrite = async function (urlImg, image) {
         $(image).attr("src", urlImg);
     }
 
     this.isCurrentPageAChapterPage = async function (doc, curUrl) {
-        return $('div#br-slider img', doc).length > 0;
+        return false
+        // This is not needed with regex?
+        return $(this.options.chapter_determine_selector, doc).length > 0 || $(this.options.chapter_determine_strip_selector, doc).length > 0 ;
     }
     this.doSomethingBeforeWritingScans = function (doc, curUrl) {
-        $('body', doc).empty()
+        // $('body', doc).empty()
+    }
+
+    /*
+        Load chapters in list mode because it relies on html instead of javascript to load the images
+    */
+    this.getStripMode = async function(doc, curUrl) {
+        if (!this.isStripMode(curUrl)) {
+            doc = await amr.loadPage(curUrl + '/strip', { nocache: true})
+        }
+        return doc;
+    }
+
+    this.stripUrl = function(curUrl) {
+        return this.isStripMode(curUrl) ? curUrl : curUrl + '/strip'
+    }
+
+    this.isStripMode = function(curUrl) {
+        let url = new URL(curUrl)
+        let path = url.pathname
+        return path.split('/').includes('strip')
     }
 }
 
