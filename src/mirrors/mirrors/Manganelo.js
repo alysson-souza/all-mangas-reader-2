1e830f4eaac129354ca2bb16e45eeab53623819c
Batosi <batosi05@gmail.com>
2020-01-02 12:47:35 -0500

    Manganelo update and toonily fix
    Updated manganelo to add the subdomain chap.
    Tooniy changed from data-src to src attribute for images

--- src/mirrors/mirrors/Manganelo.js
+++ src/mirrors/mirrors/Manganelo.js
@@ -3,7 +3,7 @@ if (typeof registerMangaObject === 'function') {
         mirrorName: "Manganelo",
         mirrorIcon: "manganelo.png",
         languages: "en",
-        domains: ["manganelo.com"],
+        domains: ["manganelo.com", "chap.manganelo.com"],
         home: "https://manganelo.com/",
         chapter_url: /^\/chapter\/.*\/.*$/g,
 
@@ -12,7 +12,7 @@ if (typeof registerMangaObject === 'function') {
             base_url: "https://manganelo.com/",
             series_list_selector: '.search-story-item a.item-title',
             chapter_list_selector: '.row-content-chapter a',
-            chapter_information_selector: '.panel-breadcrumb:first a[href*="/manga/"]:first',
+            chapter_information_selector: '.panel-breadcrumb:first a[href*="/manga/"]:first, .panel-breadcrumb:first a[href*="/manga-"]:first',
             images_selector: '.container-chapter-reader img'
         }
         
