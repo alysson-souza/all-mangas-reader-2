89eb2d01e8f2bea90406d11b7a1c2c73bdab4fc2
Batosi <batosi05@gmail.com>
2020-07-05 10:30:35 -0400

    Couple of fixes

--- src/mirrors/mirrors/EarlyManga.js
+++ src/mirrors/mirrors/EarlyManga.js
@@ -3,13 +3,13 @@ if (typeof registerMangaObject === 'function') {
         mirrorName: "EarlyManga",
         mirrorIcon: "earlymanga.png",
         languages: "en",
-        domains: ["earlymanga.net"],
-        home: "https://earlymanga.net",
+        domains: ["earlymanga.net", "earlymanga.me"],
+        home: "https://earlymanga.me",
         chapter_url: /^\/manga\/.*\/.*$/g,
 
         abstract: "Madara",
         abstract_options: {
-            search_url: "https://earlymanga.net/",
+            search_url: "https://earlymanga.me/",
             search_json: true,
             img_src: "data-src",
             secondary_img_src: "src",
