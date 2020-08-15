9e6b3c0ce9819aa4c0416ca651395bbaa0e2a40f
Batosi <batosi05@gmail.com>
2020-08-15 14:19:04 -0400

    Testing discord webook
    Testing out the webhook for discord
    
    
    Newlines might be a problem though

---
+++ scripts/discord-notify.js
@@ -0,0 +1,53 @@
+const webhook = require("webhook-discord")
+
+let id, token, subject, body, version
+
+/* Build variables from arguments */
+const idIndex = process.argv.indexOf('--id')
+if (idIndex > -1) {
+    id = process.argv[idIndex + 1]
+}
+
+const tokenIndex = process.argv.indexOf('--token')
+if (tokenIndex > -1) {
+    token = process.argv[tokenIndex + 1]
+}
+
+const subjectIndex = process.argv.indexOf('--subject')
+if (subjectIndex > -1) {
+    subject = process.argv[subjectIndex + 1]
+}
+
+const bodyIndex = process.argv.indexOf('--body')
+if (bodyIndex > -1) {
+    body = process.argv[bodyIndex + 1]
+}
+
+const versionIndex = process.argv.indexOf('--version')
+if (versionIndex > -1) {
+    version = process.argv[versionIndex + 1]
+}
+
+if (!id || !token || !subject || !body || !version) {
+    console.log('Invalid params')
+    process.exit(255)
+}
+
+let url = `https://discordapp.com/api/webhooks/${id}/${token}`
+
+
+
+try {
+    let hook = new webhook.Webhook(url)
+    let message = new webhook.MessageBuilder()
+        .setName('Beta Builder')
+        .setColor('#04ff00')
+        .setText(`@everyone Beta version ${version} is now available`)
+        .addField(subject, body)
+
+    hook.send(message)
+
+    
+} catch(error) {
+    console.log(error)
+}
