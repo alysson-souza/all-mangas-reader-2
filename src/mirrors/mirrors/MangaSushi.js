0e48e98c776eff942d66a9f7caecee6de604274d
Batosi <batosi05@gmail.com>
2020-08-26 19:23:25 -0400

    Fixed Scan FR
    Fixed Scan FR for the new domain name
    Added Manga Sushi

---
+++ src/mirrors/mirrors/MangaSushi.js
@@ -0,0 +1,17 @@
+if (typeof registerMangaObject === 'function') {
+    registerMangaObject({
+        mirrorName: "Manga Sushi",
+        mirrorIcon: "mangasushi.png",
+        languages: "en",
+        domains: ["mangasushi.net"],
+        home: "https://mangasushi.net/",
+        chapter_url: /^\/manga\/.*\/.*$/g,
+        canListFullMangas: false,
+        abstract: "Madara",
+        abstract_options: {
+            search_url: "https://mangasushi.net/",
+            secondary_img_src: "data-src",
+            chapter_list_ajax: true
+        }
+    })
+}
--- src/mirrors/mirrors/ScanFR.js
+++ src/mirrors/mirrors/ScanFR.js
@@ -3,14 +3,14 @@ if (typeof registerMangaObject === 'function') {
         mirrorName: "ScanFR",
         mirrorIcon: "scanfr.png",
         languages: "fr",
-        domains: ["www.scan-fr.co", "scan-fr.co"],
-        home: "https://www.scan-fr.co",
+        domains: ["www.scan-fr.co", "scan-fr.co", "www.scan-fr.cc"],
+        home: "https://www.scan-fr.cc",
         chapter_url: /^\/manga\/.*\/.*$/g,
         
         abstract: "MyMangaReaderCMS",
         abstract_options: {
-            base_url: "https://www.scan-fr.co",
-            chapters_element: "ul.chapterszz a[href*='/manga/']"
+            base_url: "https://www.scan-fr.cc",
+            chapters_element: "ul.chapters888 a[href*='/manga/']"
         }
     })
 }
