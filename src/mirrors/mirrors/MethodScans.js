d930c970b1591e6f30d1cf8b644aafd60253e05c
Batosi <batosi05@gmail.com>
2020-08-27 17:58:00 -0400

    Adding Method Scans

---
+++ src/mirrors/mirrors/MethodScans.js
@@ -0,0 +1,15 @@
+if (typeof registerMangaObject === 'function') {
+    registerMangaObject({
+        mirrorName: 'Method Scans',
+        mirrorIcon: 'methodscans.png',
+        languages: 'en',
+        domains: ['methodscans.com'],
+        home: 'https://methodscans.com/home',
+        canListFullMangas: true,
+        chapter_url: /^\/comics\/.*\/.*$/g,
+        abstract: 'GenkanAbs',
+        abstract_options: {
+            base_url: 'https://methodscans.com/',
+        },
+    });
+}
