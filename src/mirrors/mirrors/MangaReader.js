f2e2f8dd062cb7969f796d42514d808458eaedb7
Batosi <batosi05@gmail.com>
2020-08-29 19:18:49 -0400

    Fix Mangareader
    Fixing Mangareader hopefully

--- src/mirrors/mirrors/MangaReader.js
+++ src/mirrors/mirrors/MangaReader.js
@@ -4,66 +4,79 @@ if (typeof registerMangaObject === 'function') {
         canListFullMangas: false,
         mirrorIcon: "mangareader.png",
         domains: ["www.mangareader.net"],
-        home: "http://www.mangareader.net/",
+        home: "http://www.mangareader.net",
         chapter_url: /\/.*\/[0-9]+.*/g,
         languages: "en",
     
         getMangaList: async function (search) {
-            let urlManga = "http://www.mangareader.net/search/?w=" + search + "&rd=0&status=0&order=0&genre=0000000000000000000000000000000000000&p=0";
-            let doc = await amr.loadPage(urlManga, { nocache: true, preventimages: true })
-            var res = [];
-            $(".manga_name a", doc).each(function (index) {
-                res[res.length] = [
+            let res = await this.searchPage(this.home + "/search/?nsearch=" + search + "&msearch=")
+            return res
+        },
+
+        searchPage: async function(url) {
+            let doc = await amr.loadPage(url, { nocache: true, preventimages: true })
+            let self = this
+            let res = []
+            $(".d57 a", doc).each(function (index) {
+                res.push([
                     $(this).text(),
-                    "http://www.mangareader.net" + $(this).attr("href")
-                ];
-            });
-            return res;
+                    self.home + $(this).attr("href")
+                ])
+            })
+
+            let nextPage = $(".d71 a:contains('>')", doc)
+            if (nextPage.length > 0) {
+                res = [...res, ...await this.searchPage(self.home + nextPage.attr('href'))]
+            }
+            return res
         },
     
         getListChaps: async function (urlManga) {
             if (urlManga.indexOf("http://") == 0) urlManga = "http://" + urlManga.substring(7);
             let doc = await amr.loadPage(urlManga, { nocache: true, preventimages: true })
-            
-            var res = [];
-            var mangaName = $("#mangaproperties h1", doc).text().replace("Manga", "").trim();
-            $("#chapterlist #listing tr td:first-child a", doc).each(function (index) {
-                res[res.length] = [
+            let self = this
+            var res = []
+            var mangaName = $(".d41 .name", doc).text().replace("Manga", "").trim();
+            $(".d48 a", doc).each(function (index) {
+                res.push([
                     $(this).text().replace(mangaName, "").trim(), 
-                    "http://www.mangareader.net" + $(this).attr("href")
-                ];
-            });
-            res = res.reverse();
-            return res;
+                    self.home + $(this).attr("href")
+                ])
+            })
+            res = res.reverse()
+            return res
         },
     
         getInformationsFromCurrentPage: async function (doc, curUrl) {
-            var name = $($("#mangainfo h2.c4 a", doc)[0]).text().replace("Manga", "").trim();
-            var mangaurl = "http://www.mangareader.net" + $($("#mangainfo h2.c4 a", doc)[0]).attr("href");
-            var curChapUrl = "http://www.mangareader.net" + $($("#mangainfo_son a", doc)[0]).attr("href");
+            let nameLink = $($(".d43 a", doc)[0])
+            var name = nameLink.text().replace("Manga", "").trim()
+            var mangaurl = this.home  + nameLink.attr("href");
             return {
                 "name": name,
                 "currentMangaURL": mangaurl,
-                "currentChapterURL": curChapUrl
+                "currentChapterURL": curUrl
             };
         },
     
         getListImages: async function (doc, curUrl) {
-            res = [];
-            $("#pageMenu option", doc).each(function (index) {
-                res[res.length] = "http://www.mangareader.net" + $(this).val();
-            });
-            return res;
+            let res = []
+            let regex = /<script>(.*?)<\/script>/g
+            let matches = regex.exec(doc.innerText)
+            let json = JSON.parse(matches[1].replace('document["mj"]=', ''))
+            console.log(json)
+
+            json.im.forEach(elem => res.push(elem.u))
+            
+            console.log(res)
+            return res
         },
     
         getImageFromPageAndWrite: async function (urlImg, image) {
-            let doc = await amr.loadPage(urlImg)
-            let src = $("#imgholder img", doc).attr("src")
-            $(image).attr("src", src)
+            $(image).attr("src", urlImg)
         },
 
         isCurrentPageAChapterPage: function (doc, curUrl) {
-            return ($("img", $("#imgholder", doc)).length !== 0);
+            return $("img#ci", doc).length > 0
         }
     })
 }
