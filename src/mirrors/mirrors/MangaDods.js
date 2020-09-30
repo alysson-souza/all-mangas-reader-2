3d3a9c3c502c99f58f9758a27fb8dc8398331271
Batosi <batosi05@gmail.com>
2020-09-30 18:13:48 -0400

    Adding Manga Dods
    Added Manga Dods (https://www.mangadods.com)

---
+++ src/mirrors/mirrors/MangaDods.js
@@ -0,0 +1,19 @@
+if (typeof registerMangaObject === 'function') {
+    registerMangaObject({
+        mirrorName: "Manga Dods",
+        mirrorIcon: "mangadods.png",
+        languages: "en",
+        domains: ["www.mangadods.com"],
+        home: "https://www.mangadods.com/",
+        chapter_url: /\/manga\/.*\/\d/g,
+        canListFullMangas: false,
+        abstract: "Madara",
+        abstract_options: {
+            search_url: "https://www.mangadods.com/",
+            img_src: "src",
+            chapter_list_ajax: false,
+            secondary_img_src: "data-src"
+        }
+    })
+}
+
