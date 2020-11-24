aeb28292684c61081891f02da09f403a8b68798b
Batosi <batosi05@gmail.com>
2020-11-24 18:40:54 -0500

    Adding Twilight Scans
    Added twilight scans (https://twilightscans.com/)

---
+++ src/mirrors/mirrors/TwilightScans.js
@@ -0,0 +1,18 @@
+if (typeof registerMangaObject === 'function') {
+    registerMangaObject({
+        mirrorName: "Twilight Scans",
+        mirrorIcon: "twilightscans.jpg",
+        languages: "en",
+        domains: ["twilightscans.com"],
+        home: "https://twilightscans.com/",
+        chapter_url: /^\/manga\/.*\/.*$/g,
+        canListFullMangas: false,
+        abstract: "Madara",
+        abstract_options: {
+            search_url: "https://twilightscans.com/",
+            chapter_list_ajax: true,
+            secondary_img_src: "data-src"
+        }
+    })
+}
+
