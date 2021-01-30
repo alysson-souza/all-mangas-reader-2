a96be625133169e83d310502b3a4b50a37b94120
Batosi <batosi05@gmail.com>
2021-01-30 12:31:55 -0500

    New mirror and fix
    * Fixed Night Comic (https://www.nightcomic.com/)
    * Added Aloalivn (https://aloalivn.com/)

---
+++ src/mirrors/mirrors/Aloalivn.js
@@ -0,0 +1,17 @@
+if (typeof registerMangaObject === 'function') {
+    registerMangaObject({
+        mirrorName: "Aloalivn",
+        mirrorIcon: "aloalivn.png",
+        languages: "en",
+        domains: ["aloalivn.com"],
+        home: "https://aloalivn.com/",
+        chapter_url: /^\/manga\/.*\/.*$/g,
+        canListFullMangas: false,
+        abstract: "Madara",
+        abstract_options: {
+            search_url: "https://aloalivn.com/",
+            chapter_list_ajax: true
+        }
+    })
+}
+
