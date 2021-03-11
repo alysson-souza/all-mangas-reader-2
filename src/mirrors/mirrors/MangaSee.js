29cfbdc2b26cc814593af81856280a7a15dc442f
Batosi <batosi05@gmail.com>
2021-03-11 16:57:23 -0500

    Fixing these two again
    Fix Manga4Life and MangaSee

--- src/mirrors/mirrors/MangaSee.js
+++ src/mirrors/mirrors/MangaSee.js
@@ -69,7 +69,8 @@ if (typeof registerMangaObject === 'function') {
             matches = regex.exec(doc.innerText)
             let vars = JSON.parse(matches[1])
 
-            regex = /vm\.CurChapter\s*=\s*\{.*?\};\s*vm\.\w+\s*=\s*"(.*?)";/g
+            // regex = /vm\.CurChapter\s*=\s*\{.*?\};\s*vm\.\w+\s*=\s*"(.*?)";/g
+            regex = /vm\.CurPathNames = "(.*?)";/g
             matches = regex.exec(doc.innerText)
             let cdnPath = matches[1]
 
