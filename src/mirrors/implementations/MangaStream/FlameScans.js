78e732e7bd9258c7850ac594753d3648e8c3711b
Batosi <batosi05@gmail.com>
2021-03-21 19:30:29 -0400

    Adding Flame Scans
    Added Flame Scans (https://flamescans.org)

---
+++ src/mirrors/implementations/MangaStream/FlameScans.js
@@ -0,0 +1,20 @@
+if (typeof registerMangaObject === 'function') {
+    registerMangaObject({
+        mirrorName: "Flame Scans",
+        canListFullMangas: false,
+        mirrorIcon: "flamescans.png",
+        domains: ["flamescans.org"],
+        home: "https://flamescans.org",
+        chapter_url: /\/.*?chapter-[0-9]+\//g,
+        languages: "en",
+        abstract: "MangastreamAbs",
+        abstract_options: {
+            search_url: "https://flamescans.org",
+            chapters_a_sel: "div.bixbox.bxcl ul li div.eph-num a",
+            chapters_text_sel: "span.chapternum",
+            search_json: false,
+            img_sel: `#readerarea img[width!="1px"][class*="size-full"]`,
+            img_src: 'src'
+        },      
+    })
+}
