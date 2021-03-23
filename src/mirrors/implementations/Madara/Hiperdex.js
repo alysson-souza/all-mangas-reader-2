0cbda0141924c30eb7506703f491a58a02d7dd7e
Batosi <batosi05@gmail.com>
2021-03-23 11:48:10 -0400

    Adding Hiperdex
    Added Hiperdex (https://hiperdex.com/)

---
+++ src/mirrors/implementations/Madara/Hiperdex.js
@@ -0,0 +1,17 @@
+if (typeof registerMangaObject === 'function') {
+    registerMangaObject({
+        mirrorName: "Hiperdex",
+        mirrorIcon: "hiperdex.png",
+        languages: "en",
+        domains: ["hiperdex.com"],
+        home: "https://hiperdex.com/",
+        chapter_url: /^\/manga\/.*\/.+$/g,
+
+        abstract: "Madara",
+        abstract_options: {
+            search_url: "https://hiperdex.com/",
+            search_json: true,
+            chapter_list_ajax: true
+        }
+    })
+}
