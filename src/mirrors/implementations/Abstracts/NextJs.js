f4c769712412e404217032308010d7069059aea9
Alysson Souza e Silva <alysson@ll9.com.br>
2021-04-12 17:37:27 +0000

    Improve CatManga and extract Abstract

---
+++ src/mirrors/implementations/Abstracts/NextJs.js
@@ -0,0 +1,60 @@
+window["NextJs"] = function (options) {
+    this.defaultOptions = {
+        nextDataRegex: /&lt;script\s+id="__NEXT_DATA__"\s+type="application\/json"&gt;(.*?)&lt;\/script&gt;/m,
+        getTitle: (json) => "",
+        getMangaList: (json) => [],
+        getChapterList: (json, mangaUrl) => [],
+        getImageList: (json) => [],
+        isChapterPage: (json, pageUrl) => false
+    };
+    this.options = Object.assign(this.defaultOptions, options);
+
+    this.getMangaList = async function (url) {
+        const doc = await amr.loadPage(url, {
+            nocache: true,
+            preventimages: true
+        });
+        const json = JSON.parse(doc['outerHTML'].match(this.options.nextDataRegex)[1]);
+        return this.options.getMangaList(json);
+    };
+
+    this.getListChaps = async function (mangaUrl) {
+        let doc = await amr.loadPage(mangaUrl, {
+            nocache: true,
+            preventimages: true
+        });
+        const json = JSON.parse(doc['outerHTML'].match(this.options.nextDataRegex)[1]);
+        return this.options.getChapterList(json, mangaUrl);
+    };
+
+    this.getInformationsFromCurrentPage = async function (doc, curUrl) {
+        let json = JSON.parse(doc['outerHTML'].match(this.options.nextDataRegex)[1]);
+
+        let mangaUrl = curUrl.split('/')
+        mangaUrl.pop()
+
+        return {
+            name: this.options.getTitle(json),
+            currentMangaURL: mangaUrl.join('/'),
+            currentChapterURL: curUrl
+        };
+    };
+
+    this.getListImages = async function (doc) {
+        const json = JSON.parse(doc['outerHTML'].match(this.options.nextDataRegex)[1]);
+        return this.options.getImageList(json);
+    };
+
+    this.getImageFromPageAndWrite = async function (urlImg, image) {
+        $(image).attr("src", urlImg);
+    };
+
+    this.isCurrentPageAChapterPage = function (doc, pageUrl) {
+        const json = JSON.parse(doc['outerHTML'].match(this.options.nextDataRegex)[1]);
+        return this.options.isChapterPage(json, pageUrl);
+    };
+};
+
+if (typeof registerAbstractImplementation === "function") {
+    registerAbstractImplementation("NextJs");
+}
