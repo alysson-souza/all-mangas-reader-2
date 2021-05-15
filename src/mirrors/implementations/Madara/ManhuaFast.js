653bd6eec59edc3a35aa1de693f9f69f4977996a
Alysson Souza e Silva <alysson@ll9.com.br>
2021-05-15 22:48:58 +0100

    ManhuaFast mirror

---
+++ src/mirrors/implementations/Madara/ManhuaFast.js
@@ -0,0 +1,18 @@
+if (typeof registerMangaObject === 'function') {
+    registerMangaObject({
+        mirrorName: "ManhuaFast",
+        mirrorIcon: "manhuafast.png",
+        languages: "en",
+        domains: ["manhuafast.com"],
+        home: "https://manhuafast.com/",
+        chapter_url: /\/manga\/.+\/chapter-.+$/g,
+        canListFullMangas: false,
+        abstract: "Madara",
+        abstract_options: {
+            search_url: "https://manhuafast.com/",
+            img_src: "src",
+            chapter_list_ajax: true,
+            secondary_img_src: "data-full-url"
+        }
+    })
+}
