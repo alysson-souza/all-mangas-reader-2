dbbaef0edc1d6f7e45244c2d3b9232c478dd6fa6
Batosi <batosi05@gmail.com>
2021-05-30 18:54:53 -0400

    Increasing timeout and new mirror
    * Added mirror Luminous Scans (https://www.luminousscans.com/)
    * Increased web-ext sign timeout to see if that helps

---
+++ src/mirrors/implementations/MangaStream/LuminousScans.js
@@ -0,0 +1,17 @@
+if (typeof registerMangaObject === 'function') {
+    registerMangaObject({
+        mirrorName: "Luminous Scans",
+        canListFullMangas: false,
+        mirrorIcon: "luminousscans.png",
+        domains: ["www.luminousscans.com", "luminousscans.com"],
+        home: "https://www.luminousscans.com/",
+        chapter_url: /chapter-[0-9]+\/$/g,
+        languages: "en",
+        abstract: "MangastreamAbs",
+        abstract_options: {
+            search_url: "https://www.luminousscans.com/",
+            chapters_a_sel: "div.bixbox.bxcl ul li div.eph-num a",
+            chapters_text_sel: "span.chapternum",
+        },
+    })
+}
