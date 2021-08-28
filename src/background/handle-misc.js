778bfd18ff5b118e51ccafdf41859fe617cedb08
JiPaix <jipai13@gmail.com>
2021-08-28 18:36:12 +0200

    moved downloadAMR from background.js for consistency

--- src/background/handle-misc.js
+++ src/background/handle-misc.js
@@ -21,33 +21,7 @@ class HandleMisc {
                 });
             case "reloadStats":
                 return Promise.resolve(/*statsEvents.reloadStats()*/)
-            case "DownloadAMR":
-                return this.DownloadAMR(message)
         }
-    }
-    async DownloadAMR(message) {
-        let url = 'https://release.allmangasreader.com/all-mangas-reader-latest.crx';
-        let filename = navigator.platform === 'Win32' ? 'all-mangas-reader-latest.7z' : 'all-mangas-reader-latest.zip'
-        if(message.beta) {
-            url = 'https://release.allmangasreader.com/all-mangas-reader-beta-latest.crx';
-            filename = navigator.platform === 'Win32' ? 'all-mangas-reader-beta-latest.7z' : 'all-mangas-reader-beta-latest.zip'
-
-        }
-        const res = await fetch(url)
-        const fileStream = streamSaver.createWriteStream(filename, {
-            size: res.headers.get('content-length')
-        })
-        const readableStream = res.body
-        if(window.WritableStream && readableStream.pipeTo) {
-            return await readableStream.pipeTo(fileStream);
-        }
-        return this.pump(fileStream.getWriter(), res.body.getReader())
-    }
-    async pump(writer, reader) {
-        const res = await reader.read()
-        if(res.done) return await writer.close()
-        await writer.write(res.value)
-        return this.pump(writer, reader)
     }
 }
 export default (new HandleMisc)
