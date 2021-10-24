1a773c00a403fde0f259feb6cb7bb930fa0078d3
JiPaix <jipai13@gmail.com>
2021-10-24 02:13:32 +0200

    removed flag converter as mg language are fixed

--- src/pages/components/Flag.vue
+++ src/pages/components/Flag.vue
@@ -22,33 +22,10 @@ export default {
     },
     computed: {
         country () {
-            // if(mirrorName !== 'MangaDex V5') return
-            let value = this.value
             if(Array.isArray(this.value)) {
-                value = this.value[0]
+                return this.value[0]
             }
-            if(value === 'pt-br') return 'br'
-            if(value === 'es-la') return 'mx'
-            if(value === 'fa') return 'ir'
-            if(value === 'cs') return 'cz'
-            if(value === 'zh-hk') return 'hk'
-            if(value === 'zh') return 'cn'
-            if(value === 'uk') return 'ua'
-            if(value === 'vi') return 'vn'
-            if(value === 'he') return 'il'
-            if(value === 'ms') return 'my'
-            if(value === 'tl') return 'ph'
-            if(value === 'ja') return 'jp'
-            if(value === 'ko') return 'kr'
-            if(value === 'hi') return 'in'
-            if(value === 'bn') return 'bd'
-            if(value === 'sv') return 'se'
-            if(value === 'el') return 'gr'
-            if(value === 'sr') return 'ba'
-            if(value === 'da') return 'dk'
-            if(value === 'ca') return 'ct'
-            if(value === 'null' || value === 'NULL') return '_United-Nations'
-            return value
+            return this.value
         },
         isBig() {
             if(this.big) return ' big'
