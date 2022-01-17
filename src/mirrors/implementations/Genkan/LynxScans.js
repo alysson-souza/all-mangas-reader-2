feb7f15da08b50fffcdd43c0facd51699baa58c3
Batosi <batosi05@gmail.com>
2022-01-17 00:37:37 -0500

    Mirror Fixes
    * Kiryuu - Url and site format change
    * Lynx Scans - Url format change

--- src/mirrors/implementations/Genkan/LynxScans.js
+++ src/mirrors/implementations/Genkan/LynxScans.js
@@ -6,11 +6,12 @@ if (typeof registerMangaObject === 'function') {
         domains: ['lynxscans.com'],
         home: 'https://lynxscans.com/home',
         canListFullMangas: true,
-        chapter_url: /^\/web\/comics\/.*\/.+$/g,
+        chapter_url: /^\/comics\/.*\/.+$/g,
         abstract: 'GenkanAbs',
         abstract_options: {
-            base_url: 'https://lynxscans.com/',
-            chapter_information_title_url_parts: 6,
+            base_url: 'https://lynxscans.com/web/',
+            chapter_information_title_url_parts: 5,
+            images_include_base_url: false
         },
     });
 }
