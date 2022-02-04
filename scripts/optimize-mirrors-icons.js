725863cff01fe9987037f2f5c063d03b48368336
JiPaix <jipai13@gmail.com>
2022-02-04 17:15:54 +0100

    refactored icon optimization
    cover a lot more cases

--- scripts/optimize-mirrors-icons.js
+++ scripts/optimize-mirrors-icons.js
@@ -2,18 +2,19 @@ const fs = require('fs')
 const path = require('path')
 const sharp = require('sharp')
 
+const { renameToOptimized, isPng, isOptimzedPng } = require('./icons-helper')
 
 let files = fs.readdirSync(path.join(__dirname, '../', 'src/mirrors/icons'))
 
 files = files.map(f => path.join(__dirname, '../', 'src/mirrors/icons', f))
 
 // filter out image without "-optimized.png" extension
-files = files.filter(f => !f.endsWith('-optimized.png'))
+files = files.filter(f => !isPng(f) || !isOptimzedPng(f))
 
 if(files.length) console.log('Optimizing '+files.length+' icons...')
 
 files.forEach(file => {
-  const newFileName = file.replace(/(\.png|\.jpg|\.gif|\.webp)$/g, '-optimized.png')
+  const newFileName = renameToOptimized(file)
   sharp(file)
     .resize(16, 16)
     .toFormat('png')
