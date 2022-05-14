b8dd18c92a87172b38576e8c7fdee4d612b4aec5
mmorain <mmorain@protonmail.com>
2022-05-14 18:17:26 +0200

    update urls and domain

--- src/mirrors/implementations/MyMangaReaderCMS/ScanFR.js
+++ src/mirrors/implementations/MyMangaReaderCMS/ScanFR.js
@@ -3,13 +3,13 @@ if (typeof registerMangaObject === "function") {
         mirrorName: "ScanFR",
         mirrorIcon: "scanfr.png",
         languages: "fr",
-        domains: ["scan-fr.cc", "www.scan-fr.cc"],
-        home: "https://www.scan-fr.cc",
+        domains: ["scan-fr.org", "www.scan-fr.org"],
+        home: "https://www.scan-fr.org",
         chapter_url: /^\/manga\/.*\/.+$/g,
 
         abstract: "MyMangaReaderCMS",
         abstract_options: {
-            base_url: "https://www.scan-fr.cc",
+            base_url: "https://www.scan-fr.org",
             chapters_element: "ul.chapterszozo a[href*='/manga/']"
         }
     })
