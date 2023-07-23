5c689965b8ccf567f6fef8a66d3bbd1611a60608
Kestutis Kasiulynas <k.kasiulynas@gmail.com>
2023-07-24 00:18:09 +0300

    Remove duplicate entry

--- src/mirrors/sites/FoolSlide/FoolSlide.ts
+++ src/mirrors/sites/FoolSlide/FoolSlide.ts
@@ -184,24 +184,6 @@ export const getFoolSlideImplementations = (mirrorHelper: MirrorHelper): MirrorI
                 base_url: "https://reader.silentsky-scans.net",
                 mglist_selector: ".title > a[href*='/series/']"
             }
-        ),
-
-        new FoolSlide(
-            mirrorHelper,
-            {
-                mirrorName: "Evil Flowers",
-                mirrorIcon: require("../../icons/evilflowers-optimized.png"),
-                languages: "en",
-                domains: ["reader.evilflowers.com"],
-                home: "http://reader.evilflowers.com",
-                canListFullMangas: false /* to avoid loading 8 pages to load all mangas */,
-                chapter_url: /^\/read\/.+$/g
-            },
-            {
-                base_url: "http://reader.evilflowers.com",
-                search_all: false /* use FoolSlide search function */,
-                mglist_selector: ".title > a[href*='/series/']"
-            }
         )
     ]
 }
