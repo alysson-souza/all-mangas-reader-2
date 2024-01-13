a664f390095791a688db2948450adc5a11c4f66a
Kestutis Kasiulynas <k.kasiulynas@gmail.com>
2024-01-13 21:06:01 +0700

    Need to convert to json before accessing message.

--- src/amr/storage/gist-storage.js
+++ src/amr/storage/gist-storage.js
@@ -27,8 +27,9 @@ export default class GistStorage extends Storage {
         }
         if (response.headers.get("x-ratelimit-remaining") === "0") {
             // Set delay according to API response
+            const data = await response.json()
             const timestamp = parseInt(response.headers["x-ratelimit-reset"]) * 1000
-            throw new ThrottleError(response.data.message, new Date(timestamp))
+            throw new ThrottleError(data.message, new Date(timestamp))
         }
 
         const type = response.headers.get("content-type")
