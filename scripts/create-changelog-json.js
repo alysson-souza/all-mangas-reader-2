9f9b96d9c87245f47ef6afc1aabe27c0f2f1bba2
Batosi <batosi05@gmail.com>
2024-05-19 17:16:53 -0400

    Changelog formatting NO-CI
    Changed the format the changelog.json file gets created and added an ignore rule for prettier so it does not always get marked as changed

--- scripts/create-changelog-json.js
+++ scripts/create-changelog-json.js
@@ -14,7 +14,7 @@ const ChangelogParser = require("changelog-parser")
 const file = fs.readFileSync(path.join(__dirname, "../CHANGELOG.md"), { encoding: "utf-8" })
 
 ChangelogParser({ text: file }).then(function (res) {
-    fs.writeFile(path.join(__dirname, "../changelog.json"), JSON.stringify(res), function (err) {
+    fs.writeFile(path.join(__dirname, "../changelog.json"), JSON.stringify(res, null, "    "), function (err) {
         if (err) return console.log(err)
         console.log("Changelog file built")
     })
